1f29ba52d3875aa05975c1e50feca7ee568e7919
Pedro <50713178+prgf87@users.noreply.github.com>
2023-12-05 14:33:33 +0000

    created login page and routes

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -3,10 +3,14 @@ import { RouteRecordRaw } from "vue-router";
 const routes: RouteRecordRaw[] = [
   {
     path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
+  },
+  {
+    path: "/auth",
     component: () => import("layouts/ExtraLayout.vue"),
     children: [
-      { path: "", component: () => import("pages/IndexPage.vue") },
-      // { path: "todo", component: () => import("pages/IndexPage.vue") },
+      { path: "login", component: () => import("pages/LoginPage.vue") },
     ],
   },
 
