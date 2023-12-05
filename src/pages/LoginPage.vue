7e76259e2412331003dbed26cb2beb6e1744f987
Pedro <50713178+prgf87@users.noreply.github.com>
2023-12-05 14:50:23 +0000

    login page with inputs and wrapped in form

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,10 +1,30 @@
 <template>
-  <q-page padding> Login Page </q-page>
+  <q-page class="flex flex-center" padding>
+    <section class="full-width" style="max-width: 400px">
+      <h5 class="text-center q-my-sm">Login</h5>
+      <q-card>
+        <q-card-section>
+          <q-form>
+            <q-input
+              model-value=""
+              label="Email"
+              type="email"
+              placeholder="info@email.com"
+              filled
+              class="q-mb-md"
+            />
+            <q-input
+              model-value=""
+              label="Password"
+              type="password"
+              placeholder=""
+              filled
+            />
+          </q-form>
+        </q-card-section>
+        <q-checkbox class="q-mb-md" label="Remember me" :model-value="true" />
+        <q-btn label="Login" class="full-width" color="primary" />
+      </q-card>
+    </section>
+  </q-page>
 </template>
-
-<script lang="ts">
-import { defineComponent } from "vue";
-export default defineComponent({
-  // name: 'PageName'
-});
-</script>
